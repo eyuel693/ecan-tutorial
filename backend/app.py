a11e51ce48e7916324ae149f5128d36ba222ee33
Eyuel <eyueyuel85@gmail.com>
2025-11-04 15:45:07 +0300

    modify ports

--- backend/app.py
+++ backend/app.py
@@ -198,5 +198,4 @@ def remove_code():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000)) # Render provides the PORT variable
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
